b3e85ae08a36c1cabee38585117d1e01ed28a15c
fayola <fayola@fayola.net>
2019-06-24 19:58:17 -0400

    seting up first fragment

---
+++ app/src/main/java/net/fayola/hydraulica/CalcFragment.java
@@ -0,0 +1,128 @@
+package net.fayola.hydraulica;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CalcFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CalcFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalcFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_AIRP = "airP";
+    private static final String ARG_RADIUS = "radius";
+
+    private String mAirP;
+    private String mRadius;
+
+    private OnFragmentInteractionListener mListener;
+
+   //UI Components
+    private EditText mAirPEdit;
+    private EditText mRadiusEdit;
+    private Button mCalcForceBtn;
+
+
+    public CalcFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param airP Parameter 1.
+     * @param radius Parameter 2.
+     * @return A new instance of fragment CalcFragment.
+     */
+    public static CalcFragment newInstance(String airP, String radius) {
+        CalcFragment fragment = new CalcFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_AIRP, airP);
+        args.putString(ARG_RADIUS, radius);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mAirP = getArguments().getString(ARG_AIRP);
+            mRadius = getArguments().getString(ARG_RADIUS);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calc, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAirPEdit = view.findViewById(R.id.entered_airp);
+        mRadiusEdit = view.findViewById(R.id.entered_radius);
+        if (getArguments() != null) {
+            mAirPEdit.setText(mAirP);
+            mRadiusEdit.setText(mRadius);
+        }
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
